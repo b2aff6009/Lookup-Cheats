9507f619fd14b4006010c49bb374cd6396c84937
b2aff6009 <b2aff6009@gmail.com>
2019-08-07 20:39:52 +0200

    Added crawler.py ...

---
+++ crawler.py
@@ -0,0 +1,36 @@
+import os
+
+class Crawler:
+    def __init__(self, settings):
+        self.settings = settings
+        self.sheets = {}
+
+    def getSheets(self):
+        for path in self.settings.get("directories", []):
+            self.searchFiles(path)
+        return self.sheets
+
+
+    def searchFiles(self, path):
+        result = []
+        for subdir, dirs, files in os.walk(path):
+            for filename in files:
+                if (filename.endswith(self.settings["extension"])):
+                    filepath = os.path.join(subdir, filename)
+                    self.sheets[filename.replace(self.settings["extension"],"")] = os.path.join(subdir, filename)
+                    #result.append([filename.replace(self.settings["extension"],""), os.path.join(subdir, filename)])
+            if self.settings["recrusive"] == False:
+                break
+        return result
+
+if __name__ == '__main__':
+    SettingsPath = "configuration.json"
+    settings = {}
+    print("Test crawler")
+    import json
+
+    with open(SettingsPath, 'rb') as f:
+        configJson = json.load(f)
+    settings = configJson["crawler"]
+    crawler = Crawler(settings)
+    print(crawler.getSheets())
