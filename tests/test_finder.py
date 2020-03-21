07341c62798c9c2be9e5c2bd22469d6ecd121618
Andreas Conrads <andreas.conrads@autodesk.com>
2020-03-21 15:10:07 +0100

    Make test working with text discover now

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -1,4 +1,3 @@
-from finder import *
 import pytest
 
 import os
@@ -8,6 +7,7 @@ current_dir = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
+from finder import *
 
 settings = {}
 
