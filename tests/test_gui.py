4971ba7b4bdb2de2a14d1c801971113a314b05f8
Andreas Conrads <Andreas.Conrads@autodesk.com>
2020-10-03 11:36:03 +0200

    Removed unneeded dependency from gui tests

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -7,7 +7,6 @@ current_dir = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
-from crawler import Crawler
 settings = {}
 import platform
 
